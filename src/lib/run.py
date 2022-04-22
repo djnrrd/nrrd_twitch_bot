7350db24eb4be14a3d84ba45c9bff61ce1803860
DJ Nrrd <djnrrd@gmail.com>
2022-04-22 08:05:43 +0100

    Renamed existing queue and handler to reflect that it's for shutting down from the TK thread

--- src/lib/run.py
+++ src/lib/run.py
@@ -8,32 +8,33 @@ from .twitch_chat import TwitchChat
 from .config import load_config
 
 
-async def message_handler(chat: TwitchChat, logger: Logger,
-                          loop: asyncio.AbstractEventLoop,
-                          message_queue: asyncio.Queue, ) -> None:
+async def shutdown_handler(chat: TwitchChat, logger: Logger,
+                           loop: asyncio.AbstractEventLoop,
+                           shutdown_queue: asyncio.Queue, ) -> None:
     """Read the Queue for messages and route them to the appropriate objects
 
     :param chat: The TwitchChat object
     :param logger: A logger object
     :param loop: The asyncio event loop
-    :param message_queue: A python queue object
+    :param shutdown_queue: A python queue object for handling shutdowns from
+        the TK thread
     """
     logger.debug('Starting message handler')
     q_loop = True
     while q_loop:
-        message = await message_queue.get()
+        message = await shutdown_queue.get()
         if message == 'SHUTDOWN':
             logger.debug('Shutdown message received')
             loop.call_soon(asyncio.create_task, chat.close())
             q_loop = False
-        message_queue.task_done()
+        shutdown_queue.task_done()
     logger.debug('Exiting message handler')
 
 
 async def async_twitch_chat_main(oauth_token: str, nickname: str, channel: str,
                                  logger: Logger,
                                  loop: asyncio.AbstractEventLoop,
-                                 message_queue: asyncio.Queue) -> None:
+                                 shutdown_queue: asyncio.Queue) -> None:
     """Run the Twitch Chat websockets client
 
     :param oauth_token: The OAuth token to log into Twitch
@@ -41,23 +42,25 @@ async def async_twitch_chat_main(oauth_token: str, nickname: str, channel: str,
     :param channel: The twitch channel to join
     :param logger: A logger object
     :param loop: The asyncio event loop
-    :param message_queue: A python asyncio Queue object
+    :param shutdown_queue: A python queue object for handling shutdowns from
+        the TK thread
     """
     async with TwitchChat(oauth_token, nickname, channel, logger) as chat:
         logger.debug('Using asyncio gather to run chatbot and queue manager')
         futures = [chat.run(),
-                   message_handler(chat, logger, loop, message_queue)]
+                   shutdown_handler(chat, logger, loop, shutdown_queue)]
         await asyncio.gather(*futures)
     logger.debug('Exiting TwitchChat and message handler')
 
 
 def twitch_chat_main(logger: Logger, loop: asyncio.AbstractEventLoop,
-                     message_queue: asyncio.Queue) -> None:
+                     shutdown_queue: asyncio.Queue) -> None:
     """Start the Twitch chat websockets client in an asyncio loop
 
     :param logger: A Logger object
     :param loop: The asyncio event loop
-    :param message_queue: A python asyncio Queue object
+    :param shutdown_queue: A python queue object for handling shutdowns from
+        the TK thread
     """
     config = load_config(logger)
     logger.debug('Starting loop')
@@ -65,20 +68,21 @@ def twitch_chat_main(logger: Logger, loop: asyncio.AbstractEventLoop,
         async_twitch_chat_main(config['twitch']['oauth_token'],
                                config['twitch']['username'],
                                config['twitch']['channel'],
-                               logger, loop, message_queue)
+                               logger, loop, shutdown_queue)
     )
     logger.debug('Ending loop')
 
 
 def run_sockets(logger: Logger, loop: asyncio.AbstractEventLoop,
-                message_queue: asyncio.Queue) -> None:
+                shutdown_queue: asyncio.Queue) -> None:
     """Create the threads to run the websocket clients and servers in
 
     :param logger: A Logger object
     :param loop: The asyncio event loop
-    :param message_queue: A python asyncio Queue object
+    :param shutdown_queue: A python queue object for handling shutdowns from
+        the TK thread
     """
-    start_chat = partial(twitch_chat_main, logger, loop, message_queue)
+    start_chat = partial(twitch_chat_main, logger, loop, shutdown_queue)
     logger.debug('Starting thread for websockets')
     chat = threading.Thread(target=start_chat, daemon=True)
     chat.start()

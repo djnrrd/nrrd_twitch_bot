6c9044ac0787dd8b8052de0e049c8a9201396f36
DJ Nrrd <djnrrd@gmail.com>
2022-04-23 13:39:49 +0100

    Add user data dir plugin folder to path

--- src/lib/plugins.py
+++ src/lib/plugins.py
@@ -23,18 +23,46 @@ def _load_from_config(logger: Logger) -> List[str]:
     return plugins.split(':')
 
 
+def _update_paths(logger: Logger) -> None:
+    """Update the sys path with the plugin locations
+
+    :param logger:
+    """
+    logger.debug('Adding local plugin path')
+    local_plugin_path = os.path.join(os.path.dirname(__file__), '..', 'plugins')
+    user_plugin_path = os.path.join(user_data_dir('nrrd-twitch-bot', 'djnrrd'),
+                                    'plugins')
+    if not os.path.isdir(user_plugin_path):
+        logger.debug('No plugin directory found, creating one')
+        # On Windows appdirs always have to be %AppDir%//author//appname so
+        # we have to create the author folder first, which would end up being
+        # empty on linux systems
+        author_dir = os.path.join(user_data_dir(), 'djnrrd')
+        if not os.path.isdir(author_dir):
+            logger.debug(f"Creating author directory: {author_dir}")
+            os.mkdir(author_dir)
+        app_dir = user_data_dir('nrrd-twitch-bot', 'djnrrd')
+        if not os.path.isdir(app_dir):
+            logger.debug(f"Creating application directory: {app_dir}")
+            os.mkdir(app_dir)
+        logger.debug(f"Creating plugin directory: {user_plugin_path}")
+        os.mkdir(user_plugin_path)
+    sys.path.extend([local_plugin_path, user_plugin_path])
+
+
 def _load_plugins(plugin_module: str, logger: Logger) -> List[ModuleType]:
     """load all plugin objects
 
     :param logger: A logger object
     :return: A list of imported packages from the list of plugins
     """
-    local_plugin_path = os.path.join(os.path.dirname(__file__), '..',
-                                     'plugins')
-    sys.path.extend([local_plugin_path])
+    _update_paths(logger)
     plugins = _load_from_config(logger)
-    return [importlib.import_module(plugin_module, x) for x in plugins
-            if importlib.util.find_spec(plugin_module, x)]
+    logger.debug(f"Importing plugin libraries supporting {plugin_module}")
+    modules = [importlib.import_module(plugin_module, x) for x in plugins
+               if importlib.util.find_spec(plugin_module, x)]
+    logger.debug(f"Loaded modules {[str(x) for x in modules]}")
+    return modules
 
 
 def load_dispatchers(logger: Logger) -> List[ModuleType]:
@@ -42,4 +70,5 @@ def load_dispatchers(logger: Logger) -> List[ModuleType]:
 
     :return: A list of dispatcher plugins
     """
+    logger.debug('Loading dispatcher plugin modules')
     return _load_plugins('.dispatcher', logger)

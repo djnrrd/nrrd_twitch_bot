ca6c57d1dda4b974fd49b55ae302fdb127813e72
DJ Nrrd <djnrrd@gmail.com>
2022-04-24 20:03:17 +0100

    Basic working examples of plugin overlay architecture

--- src/nrrd_twitch_bot/plugins/chat_overlay/overlay.py
+++ src/nrrd_twitch_bot/plugins/chat_overlay/overlay.py
@@ -1,5 +1,15 @@
-from aiohttp.web import Request, Response
+from typing import Union
+from aiohttp.web import Request, Response, FileResponse, StreamResponse
+import os
 
 
-async def handler(request: Request) -> Response:
-    return Response(text=f"OK: {request.match_info['path']}")
+async def handler(request: Request) \
+        -> Union[Response, FileResponse, StreamResponse]:
+    base_path = os.path.join(os.path.dirname(__file__), 'static')
+    if request.match_info['path'] == '' or request.match_info['path'] == '/' \
+            or request.match_info['path'] == 'index.html':
+        return FileResponse(path=os.path.join(base_path, 'thanks.html'))
+    elif request.match_info['path'] == 'thanks.js':
+        header = {'Content-type': 'application/ecmascript'}
+        return FileResponse(path=os.path.join(base_path, 'thanks.js'),
+                            headers=header)

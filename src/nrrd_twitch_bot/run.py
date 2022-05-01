5cc1debe36960127b20d0c7ef3d6101264b9180f
DJ Nrrd <djnrrd@gmail.com>
2022-05-01 10:23:42 +0100

    Reformat of debugging messages

--- src/nrrd_twitch_bot/run.py
+++ src/nrrd_twitch_bot/run.py
@@ -27,19 +27,21 @@ async def shutdown_handler(chat: TwitchChat, dispatcher: Dispatcher,
     :param shutdown_queue: A python queue object for handling shutdowns from
         the TK thread
     """
-    logger.debug('Starting message handler')
+    logger.debug('run.py: Starting shutdown handler')
     q_loop = True
     while q_loop:
         message = await shutdown_queue.get()
         if message[1] == 'SHUTDOWN':
-            logger.debug('Shutdown handler message received')
-            logger.info('Shutting down local HTTP Server')
+            logger.debug('run.py: Shutdown message received')
+            logger.debug('run.py: Shutting down local HTTP Server')
             await site.stop()
+            logger.debug('run.py: Shutting down dispatcher')
             loop.call_soon(asyncio.create_task, dispatcher.shutdown())
+            logger.debug('run.py: Shutting down chat')
             loop.call_soon(asyncio.create_task, chat.close())
             q_loop = False
         shutdown_queue.task_done()
-    logger.debug('Exiting message handler')
+    logger.debug('run.py: Exiting shutdown handler')
 
 
 async def async_main(oauth_token: str, nickname: str, channel: str,
@@ -60,23 +62,28 @@ async def async_main(oauth_token: str, nickname: str, channel: str,
     chat_rcv_queue = asyncio.PriorityQueue()
     chat_send_queue = asyncio.PriorityQueue()
     # Load twitch chat web sockets client and open the connection
+    logger.debug('run.py: Loading TwitchChat')
     chat = TwitchChat(oauth_token, nickname, channel, logger, chat_rcv_queue)
     await chat.open()
     # Gather the plugins
+    logger.debug('run.py: Loading plugins')
     plugins = load_plugins(chat_send_queue, logger)
     # Initialise the dispatcher
+    logger.debug('run.py: Loading Dispatcher')
     dispatcher = Dispatcher(chat, chat_rcv_queue, chat_send_queue,
                             plugins, logger)
     # Initialise the HTTP server
+    logger.debug('run.py: Starting aiohttp server')
     http_site = await run_http_server(plugins, logger)
-    logger.debug('Using asyncio gather to run tasks')
+    logger.debug('run.py: Using asyncio gather to run chat, dispatcher and '
+                 'shutdown handler')
     futures = [chat.run(),
                dispatcher.chat_receive(),
                dispatcher.chat_send(),
                shutdown_handler(chat, dispatcher, http_site, logger, loop,
                                 shutdown_queue)]
     await asyncio.gather(*futures)
-    logger.debug('Exiting tasks')
+    logger.debug('run.py: All asyncio tasks')
 
 
 def start_asyncio_loop(logger: Logger, loop: asyncio.AbstractEventLoop,
@@ -89,14 +96,14 @@ def start_asyncio_loop(logger: Logger, loop: asyncio.AbstractEventLoop,
         the TK thread
     """
     config = load_config(logger)
-    logger.debug('Starting asyncio event loop')
+    oauth = config['twitch']['oauth_token']
+    username = config['twitch']['username']
+    channel = config['twitch']['channel']
+    logger.debug('run.py: Starting asyncio event loop')
     loop.run_until_complete(
-        async_main(config['twitch']['oauth_token'],
-                   config['twitch']['username'],
-                   config['twitch']['channel'],
-                   logger, loop, shutdown_queue)
+        async_main(oauth, username, channel, logger, loop, shutdown_queue)
     )
-    logger.debug('Ending asyncio event loop')
+    logger.debug('run.py: Ending asyncio event loop')
 
 
 def start_new_thread(logger: Logger, loop: asyncio.AbstractEventLoop,
@@ -109,6 +116,6 @@ def start_new_thread(logger: Logger, loop: asyncio.AbstractEventLoop,
         the TK thread
     """
     start_chat = partial(start_asyncio_loop, logger, loop, shutdown_queue)
-    logger.debug('Starting thread for websockets')
+    logger.debug('run.py: Starting thread for websockets')
     chat = threading.Thread(target=start_chat, daemon=True)
     chat.start()

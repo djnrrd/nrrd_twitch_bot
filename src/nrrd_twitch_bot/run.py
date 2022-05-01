603d49506477495678e45151571b1cf5c39ceb71
DJ Nrrd <djnrrd@gmail.com>
2022-05-01 10:47:15 +0100

    Use asyncio create_task instead of loop.call_soon to shutdown the asyncio apps

--- src/nrrd_twitch_bot/run.py
+++ src/nrrd_twitch_bot/run.py
@@ -4,7 +4,7 @@ import threading
 from functools import partial
 from logging import Logger
 from aiohttp.web import TCPSite
-import asyncio
+from asyncio import create_task, PriorityQueue, AbstractEventLoop, gather
 from nrrd_twitch_bot.lib.twitch_chat import TwitchChat
 from nrrd_twitch_bot.lib.config import load_config
 from nrrd_twitch_bot.lib.dispatcher import Dispatcher
@@ -14,8 +14,7 @@ from nrrd_twitch_bot.lib.http_server import run_http_server
 
 async def shutdown_handler(chat: TwitchChat, dispatcher: Dispatcher,
                            site: TCPSite, logger: Logger,
-                           loop: asyncio.AbstractEventLoop,
-                           shutdown_queue: asyncio.PriorityQueue) -> None:
+                           shutdown_queue: PriorityQueue) -> None:
     """Read the Shutdown Queue waiting for the shutdown message and shutdown
     the asyncio operations
 
@@ -23,7 +22,6 @@ async def shutdown_handler(chat: TwitchChat, dispatcher: Dispatcher,
     :param dispatcher: The BotDispatcher object
     :param site: The local HTTP Server
     :param logger: A logger object
-    :param loop: The asyncio event loop
     :param shutdown_queue: A python queue object for handling shutdowns from
         the TK thread
     """
@@ -33,12 +31,12 @@ async def shutdown_handler(chat: TwitchChat, dispatcher: Dispatcher,
         message = await shutdown_queue.get()
         if message[1] == 'SHUTDOWN':
             logger.debug('run.py: Shutdown message received')
-            logger.debug('run.py: Shutting down local HTTP Server')
+            logger.info('run.py: Shutting down local HTTP Server')
             await site.stop()
             logger.debug('run.py: Shutting down dispatcher')
-            loop.call_soon(asyncio.create_task, dispatcher.shutdown())
+            create_task(dispatcher.shutdown())
             logger.debug('run.py: Shutting down chat')
-            loop.call_soon(asyncio.create_task, chat.close())
+            create_task(chat.close())
             q_loop = False
         shutdown_queue.task_done()
     logger.debug('run.py: Exiting shutdown handler')
@@ -46,21 +44,19 @@ async def shutdown_handler(chat: TwitchChat, dispatcher: Dispatcher,
 
 async def async_main(oauth_token: str, nickname: str, channel: str,
                      logger: Logger,
-                     loop: asyncio.AbstractEventLoop,
-                     shutdown_queue: asyncio.PriorityQueue) -> None:
+                     shutdown_queue: PriorityQueue) -> None:
     """Run the asyncio tasks in parallel
 
     :param oauth_token: The OAuth token to log into Twitch chat
     :param nickname: The twitch nickname to log into Twitch chat as
     :param channel: The twitch channel to join for to Twitch chat
     :param logger: A logger object
-    :param loop: The asyncio event loop
     :param shutdown_queue: A python queue object receiving shutdowns from
         the main TK thread
     """
     # Setup other async queues
-    chat_rcv_queue = asyncio.PriorityQueue()
-    chat_send_queue = asyncio.PriorityQueue()
+    chat_rcv_queue = PriorityQueue()
+    chat_send_queue = PriorityQueue()
     # Load twitch chat web sockets client and open the connection
     logger.debug('run.py: Loading TwitchChat')
     chat = TwitchChat(oauth_token, nickname, channel, logger, chat_rcv_queue)
@@ -80,14 +76,15 @@ async def async_main(oauth_token: str, nickname: str, channel: str,
     futures = [chat.run(),
                dispatcher.chat_receive(),
                dispatcher.chat_send(),
-               shutdown_handler(chat, dispatcher, http_site, logger, loop,
-                                shutdown_queue)]
-    await asyncio.gather(*futures)
+               shutdown_handler(chat, dispatcher, http_site, logger,
+                                shutdown_queue)
+               ]
+    await gather(*futures)
     logger.debug('run.py: All asyncio tasks')
 
 
-def start_asyncio_loop(logger: Logger, loop: asyncio.AbstractEventLoop,
-                       shutdown_queue: asyncio.PriorityQueue) -> None:
+def start_asyncio_loop(logger: Logger, loop: AbstractEventLoop,
+                       shutdown_queue: PriorityQueue) -> None:
     """Start the asyncio event loop
 
     :param logger: A Logger object
@@ -101,13 +98,13 @@ def start_asyncio_loop(logger: Logger, loop: asyncio.AbstractEventLoop,
     channel = config['twitch']['channel']
     logger.debug('run.py: Starting asyncio event loop')
     loop.run_until_complete(
-        async_main(oauth, username, channel, logger, loop, shutdown_queue)
+        async_main(oauth, username, channel, logger, shutdown_queue)
     )
     logger.debug('run.py: Ending asyncio event loop')
 
 
-def start_new_thread(logger: Logger, loop: asyncio.AbstractEventLoop,
-                     shutdown_queue: asyncio.PriorityQueue) -> None:
+def start_new_thread(logger: Logger, loop: AbstractEventLoop,
+                     shutdown_queue: PriorityQueue) -> None:
     """Create a new thread to run the asyncio tasks in
 
     :param logger: A Logger object

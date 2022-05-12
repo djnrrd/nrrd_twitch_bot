e33182a238327a4f41ad932a66e9f83320225adf
DJ Nrrd <djnrrd@gmail.com>
2022-05-12 21:05:22 +0100

    Update logging levels and docstrings, add task creator for plugin tasks

--- src/nrrd_twitch_bot/run.py
+++ src/nrrd_twitch_bot/run.py
@@ -1,6 +1,6 @@
-"""Run the main web server and websocket components in their threads
+"""Run the main web server and websocket components as asyncio tasks
 """
-from typing import List, Union
+from typing import List, Union, Type
 from logging import Logger
 import asyncio
 import signal
@@ -60,6 +60,7 @@ async def run_dispatcher(chat_rcv_queue: PriorityQueue,
     :param logger: A logger object
     """
     dispatcher = Dispatcher(chat_rcv_queue, chat_send_queue, plugins, logger)
+    logger.debug('run.py: running dispatcher')
     await dispatcher.run()
 
 
@@ -70,20 +71,31 @@ async def run_http(plugins: List[BasePlugin], logger: Logger) -> None:
     :param logger: A logger object
     """
     async with OverlayServer(plugins, logger) as http:
+        logger.debug('run.py: running http server')
         await http.run()
+
+
+async def run_plugins(plugin: Type[BasePlugin], logger: Logger) -> None:
+    """Run any plugins that implement the run() method
+
+    :param plugin: The plugin object
+    :param logger:
+    """
+    if hasattr(plugin, 'run'):
+        logger.debug(f"run.py: running plugin {plugin}")
+        await plugin.run()
 
 
 async def shutdown(loop: asyncio.AbstractEventLoop, logger: Logger) \
         -> None:
     """Gracefully Shutdown the services
 
-    :param sig:
-    :param loop:
-    :param logger:
+    :param loop: The asyncio event loop
+    :param logger: A logger object
     """
     tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
     [task.cancel() for task in tasks]
-    logger.info(f"Cancelling {len(tasks)} outstanding tasks")
+    logger.debug(f"run.py: Cancelling {len(tasks)} outstanding tasks")
     await asyncio.gather(*tasks, return_exceptions=True)
     loop.stop()
 
@@ -91,15 +103,29 @@ async def shutdown(loop: asyncio.AbstractEventLoop, logger: Logger) \
 async def shutdown_on_event(shutdown_event: Event,
                             loop: asyncio.AbstractEventLoop,
                             logger: Logger) -> None:
+    """Listen for an asyncio event from the TK app to trigger a shutdown
+
+    :param shutdown_event: The asyncio Event
+    :param loop: The asyncio event loop
+    :param logger: A logger object
+    """
     await shutdown_event.wait()
+    logger.debug('run.py: Shutdown event received')
     await shutdown(loop, logger)
     shutdown_event.clear()
 
 
-def register_signal_handlers(loop, logger):
+def register_signal_handlers(loop: asyncio.AbstractEventLoop,
+                             logger: Logger) -> None:
+    """Register shutdown signals and the shutdown handlers to the event loop
+
+    :param loop: The asyncio event loop
+    :param logger: A logger object
+    """
     # Register the signal handlers
     signals = (signal.SIGHUP, signal.SIGTERM, signal.SIGINT)
     for sig in signals:
+        logger.debug(f"run.py: Adding handler for signal {sig}")
         loop.add_signal_handler(
             sig, lambda: asyncio.create_task(shutdown(loop, logger))
         )
@@ -107,6 +133,12 @@ def register_signal_handlers(loop, logger):
 
 def run_async_tasks(logger: Logger, shutdown_event: Union[Event, None] = None) \
         -> None:
+    """Start all the services as asyncio tasks and run the event loop
+
+    :param logger: A logger object
+    :param shutdown_event: An asyncio Event object used by the main TK thread
+        to signal a shutdown of the event loop
+    """
     # Setup async queues
     chat_rcv_queue = PriorityQueue()
     chat_send_queue = PriorityQueue()
@@ -114,7 +146,6 @@ def run_async_tasks(logger: Logger, shutdown_event: Union[Event, None] = None) \
     plugins = load_plugins(logger)
     # Get the loop
     loop = asyncio.new_event_loop()
-
     try:
         loop.create_task(run_chat(chat_rcv_queue, chat_send_queue, logger))
         loop.create_task(run_dispatcher(chat_rcv_queue, chat_send_queue,
@@ -124,9 +155,8 @@ def run_async_tasks(logger: Logger, shutdown_event: Union[Event, None] = None) \
             loop.create_task(shutdown_on_event(shutdown_event, loop, logger))
         else:
             register_signal_handlers(loop, logger)
-        # futures += [x.run() for x in plugins if hasattr(x, 'run')]
-        # await gather(*futures)
+        for plugin in plugins:
+            loop.create_task(run_plugins(plugin, logger))
         loop.run_forever()
     finally:
         loop.close()
-

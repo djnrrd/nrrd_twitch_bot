c984ac3a7eeb2bc66ed1897f856ee714f32f09af
DJ Nrrd <djnrrd@gmail.com>
2022-05-17 18:11:23 +0100

    Add basic Twitch Helix API To get lists of emote sets

---
+++ src/nrrd_twitch_bot/lib/twitch_helix.py
@@ -0,0 +1,42 @@
+"""Interact with the Twitch Helix API
+"""
+import asyncio
+from typing import List
+from logging import Logger
+from basewebapi.asyncbasewebapi import AsyncBaseWebAPI
+from .config import load_config
+
+
+class TwitchHelix(AsyncBaseWebAPI):
+
+    def __init__(self, client_id: str, oauth_token: str) -> None:
+        super().__init__('api.twitch.tv', '', '', secure=True)
+        self.headers['Client-Id'] = client_id
+        self.headers['Authorization'] = f"Bearer {oauth_token}"
+
+    async def get_emote_set(self, emote_set_id: str) -> List:
+        """Get an emote set from the Twitch Helix API
+
+        :param emote_set_id: The ID of the emote set
+        :return: A list of Emotes
+        """
+        path = '/helix/chat/emotes/set'
+        params = {'emote_set_id': str(emote_set_id)}
+        result = await self._transaction('get', path, params=params)
+        return result.get('data')
+
+
+async def get_emote_sets(emote_set_ids: List[str], logger: Logger) -> List:
+    """Get an emote set from the Twitch Helix API
+
+    :param emote_set_ids: The IDs of the emote sets
+    :param logger: A logger object
+    :return: A list of Emotes
+    """
+    config = load_config(logger)
+    oauth_token = config['twitch']['oauth_token']
+    client_id = '6vhqfh4gs5ao0jcvss9ft1248kcaqq'
+    async with TwitchHelix(client_id, oauth_token) as twitch:
+        futures = [twitch.get_emote_set(x) for x in emote_set_ids]
+        emote_sets = await asyncio.gather(*futures)
+    return emote_sets

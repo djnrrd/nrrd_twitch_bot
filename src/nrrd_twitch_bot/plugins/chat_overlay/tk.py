670ab73e89aa19d3324f3bf973a63f660a8c5a8a
DJ Nrrd <djnrrd@gmail.com>
2022-05-31 10:17:43 +0100

    Option for alejo.io pronouns

--- src/nrrd_twitch_bot/plugins/chat_overlay/tk.py
+++ src/nrrd_twitch_bot/plugins/chat_overlay/tk.py
@@ -19,6 +19,7 @@ class PluginOptions(tk.Frame):
         self.chat_font = tk.StringVar()
         self.font_size = tk.StringVar()
         self.font_colour = tk.StringVar()
+        self.pronoun_option = tk.BooleanVar()
         self._setup_app()
         self._build_form()
         self._load_config_values()
@@ -28,8 +29,9 @@ class PluginOptions(tk.Frame):
         self.grid_rowconfigure(1, weight=1)
         self.grid_rowconfigure(2, weight=0)
         self.grid_rowconfigure(3, weight=0)
-        self.grid_rowconfigure(4, weight=1)
-        self.grid_rowconfigure(5, weight=0)
+        self.grid_rowconfigure(4, weight=0)
+        self.grid_rowconfigure(5, weight=1)
+        self.grid_rowconfigure(6, weight=0)
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
@@ -53,6 +55,9 @@ class PluginOptions(tk.Frame):
         colour_picker = tk.Button(self, text='Select colour',
                                   command=self._font_colour_chooser,
                                   name='font_colour', state='normal')
+        # Pronouns options
+        pronouns_label = tk.Label(self, text='Display alejo.io pronouns')
+        pronouns_option = tk.Checkbutton(self, variable=self.pronoun_option)
         # Save Button
         save_config_btn = tk.Button(self, text='Save',
                                     command=self._save_config_values,
@@ -66,7 +71,9 @@ class PluginOptions(tk.Frame):
         colour_label.grid(row=3, column=0, sticky='es', pady=5, padx=5)
         colour_box.grid(row=3, column=1, sticky='ws', pady=5, padx=5)
         colour_picker.grid(row=3, column=1, sticky='s', pady=5, padx=5)
-        save_config_btn.grid(row=5, column=1, sticky='es', pady=5, padx=5)
+        pronouns_label.grid(row=4, column=0, sticky='es', pady=5, padx=5)
+        pronouns_option.grid(row=4, column=1, sticky='ws', pady=5, padx=5)
+        save_config_btn.grid(row=6, column=1, sticky='es', pady=5, padx=5)
 
     def _save_config_values(self) -> None:
         """Save the Twitch OAuth values to the config file
@@ -75,6 +82,7 @@ class PluginOptions(tk.Frame):
         config['DEFAULT']['chat_font'] = self.chat_font.get()
         config['DEFAULT']['font_size'] = self.font_size.get()
         config['DEFAULT']['font_colour'] = self.font_colour.get()
+        config['DEFAULT']['pronoun_option'] = str(self.pronoun_option.get())
         save_config(config, 'chat_overlay.ini')
 
     def _load_config_values(self) -> None:
@@ -84,6 +92,8 @@ class PluginOptions(tk.Frame):
         self.chat_font.set(config['DEFAULT'].get('chat_font', 'Arial'))
         self.font_size.set(config['DEFAULT'].get('font_size', '16px'))
         self.font_colour.set(config['DEFAULT'].get('font_colour', 'white'))
+        self.pronoun_option.set(eval(config['DEFAULT'].get('pronoun_option',
+                                                           True)))
 
     def _font_colour_chooser(self) -> None:
         colour = askcolor(title='Default Font Colour')

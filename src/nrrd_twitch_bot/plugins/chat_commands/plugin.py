427ef7b796dd84f9437a22868538c54878cc07e4
DJ Nrrd <djnrrd@gmail.com>
2022-06-10 17:34:42 +0100

    Added user level checking

--- src/nrrd_twitch_bot/plugins/chat_commands/plugin.py
+++ src/nrrd_twitch_bot/plugins/chat_commands/plugin.py
@@ -1,6 +1,6 @@
 """An example plugin to provide a basic chat commands bot
 """
-from typing import Dict, Union
+from typing import Dict, Union, List
 from logging import Logger
 import os
 import asyncio
@@ -26,15 +26,26 @@ class ChatCommands(BasePlugin):
         # Make sure this is a command before any further processing
         if message['msg_text'][0] != '!':
             return
+        user_levels = ['broadcaster', 'moderator', 'vip', 'subscriber', 'all']
+        if 'broadcaster' in message['badges']:
+            pass
+        elif message['mod'] == '1':
+            user_levels = user_levels[1:]
+        elif 'vip' in message['badges']:
+            user_levels = user_levels[2:]
+        elif message['subscriber'] == '1':
+            user_levels = user_levels[3:]
+        else:
+            user_levels = user_levels[4:]
         parts = message['msg_text'].split(' ')
         command = parts[0][1:]
         args = parts[1:]
         self.logger.debug(f"chat_commands: command {command} received with "
                           f"args {args}")
         if command == 'command':
-            response = self.manage_commands(*args)
+            response = self.manage_commands(user_levels, *args)
         else:
-            response = self.lookup_commands(command, *args)
+            response = self.lookup_commands(command, user_levels, *args)
         if response:
             self.logger.debug(f"chat_commands: response is {response}")
             asyncio.create_task(self.dispatcher.chat_send(response))
@@ -51,6 +62,7 @@ class ChatCommands(BasePlugin):
         with load_db:
             init_table = '''CREATE TABLE IF NOT EXISTS chat_commands (
                                 command TEXT NOT NULL,
+                                level TEXT NOT NULL,
                                 response TEXT NOT NULL);'''
             init_index = '''CREATE UNIQUE INDEX IF NOT EXISTS command_idx 
                             ON chat_commands(command);'''
@@ -59,36 +71,51 @@ class ChatCommands(BasePlugin):
             load_db.execute(init_index)
         return load_db
 
-    def lookup_commands(self, command: str, *args) -> Union[str, None]:
+    def lookup_commands(self, command: str, user_levels: List, *args) \
+            -> Union[str, None]:
         """Return the response to the chat commands
 
         :param command: The extracted command
+        :param user_levels: The user's user levels
         :param args: The list of arguments
         :return: The command response if found
         """
         with self.db_conn as con:
-            response = con.execute('SELECT response FROM chat_commands WHERE '
-                                   'command = ?', (command, ))
+            response = con.execute('SELECT response, level FROM chat_commands '
+                                   'WHERE command = ?', (command, ))
             result = response.fetchall()
             if len(result) == 1:
+                level = result[0][1]
+                if level not in user_levels:
+                    return
                 self.logger.debug(f"chat_commands: Found entry for {command} "
                                   f"in sqlite3 dB")
                 return result[0][0].format(*args)
 
-    def manage_commands(self, *args) -> Union[str, None]:
+    def manage_commands(self, user_levels: List, *args) -> Union[str, None]:
         """Add or delete commands to the database
 
+        :param user_levels: The user's user levels
         :param args: The list of arguments to the 'command' command
         :return: The command response
         """
+        all_levels = ['broadcaster', 'moderator', 'vip', 'subscriber', 'all']
+        usage = 'Usage: !command add|delete command_name ' \
+                'broadcaster|moderator|vip|subscriber|all {response}'
+        if 'moderator' not in user_levels:
+            return 'Mod use only'
         if len(args) >= 2:
             if args[0] == 'add':
-                if len(args) >= 3:
+                if len(args) >= 4:
                     command = args[1]
-                    response = ' '.join(args[2:])
+                    level = args[2]
+                    if level not in all_levels:
+                        return usage
+                    response = ' '.join(args[3:])
                     with self.db_conn as con:
-                        con.execute('INSERT INTO chat_commands VALUES (?, ?);',
-                                    (command, response))
+                        con.execute('INSERT INTO chat_commands '
+                                    'VALUES (?, ?, ?);',
+                                    (command, level, response))
                     return f"Added command {command}"
             elif args[0] == 'delete':
                 command = args[1]
@@ -96,4 +123,4 @@ class ChatCommands(BasePlugin):
                     con.execute('DELETE FROM chat_commands WHERE command = ?;',
                                 (command, ))
                 return f"Deleted command {command}"
-        return 'Usage: !command add|delete command_name {response}'
+        return usage
